f133d172bd894ead89f984317f30d1595f421f1b
Preston Arnold <prestonarnold19@gmail.com>
2026-04-27 03:10:29 +0100

    Export store

--- src/core/index.ts
+++ src/core/index.ts
@@ -1,3 +1,4 @@
 export type * from "./types";
 export * from "./state";
 export * from "./selectors";
+export * from "./store";
--- src/index.ts
+++ src/index.ts
@@ -8,3 +8,27 @@ export type {
   FamilyCtxSession,
   FamilyCtxState,
 } from "./core/types";
+
+export {
+  createInitialState,
+  setAccount,
+  setProfiles,
+  addProfile,
+  removeProfile,
+  startSession,
+  switchProfile,
+  enableParentMode,
+  disableParentMode,
+  clearSession,
+} from "./core/state";
+
+export {
+  getActiveProfile,
+  getParentProfile,
+  getChildProfiles,
+  isParentMode,
+  canSwitchToProfile,
+} from "./core/selectors";
+
+export { createStore } from "./core/store";
+export type { Store, Listener } from "./core/store";
